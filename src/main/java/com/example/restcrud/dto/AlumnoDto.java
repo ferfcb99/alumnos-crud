2d0cf90854408d1d98085eb2d625ea0c05cb65ff
MAXO237 <ricardo.maximiliano.vic.mor@gmail.com>
2025-06-19 16:27:19 -0600

    Alumno y AlumnoDto terminados

---
+++ src/main/java/com/example/restcrud/dto/AlumnoDto.java
@@ -0,0 +1,71 @@
+package com.example.restcrud.dto;
+
+public class AlumnoDto {
+
+    private int id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private int edad;
+    private String carrera;
+
+    public AlumnoDto(int id, String nombre, String apellidoPaterno, String apellidoMaterno, int edad, String carrera) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.edad = edad;
+        this.carrera = carrera;
+    }
+
+    public AlumnoDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
---
+++ src/main/java/com/example/restcrud/entity/Alumno.java
@@ -0,0 +1,98 @@
+package com.example.restcrud.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Table(name="alumno")
+@Entity
+public class Alumno {
+    @Column(name = "id")
+    @Id
+    private int id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido_paterno")
+    private String apellidoPaterno;
+    @Column(name = "apellido_materno")
+    private String apellidoMaterno;
+    @Column(name = "edad")
+    private int edad;
+    @Column(name = "carrera")
+    private String carrera;
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+    public Alumno(int id, String nombre, String apellidoPaterno, String apellidoMaterno, int edad, String carrera, LocalDateTime fechaCreacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Alumno() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+}
+
+
